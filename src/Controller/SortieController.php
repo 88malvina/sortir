c22e65b2615d9d5e294196fc8500eb8a8373ff46
Priscila Conchou <priscilaguedesdantas@gmail.com>
2021-08-28 14:23:19 +0200

    correction routes

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -17,10 +17,14 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\User\UserInterface;
 
+
+/**
+ * @Route("/sortie", name="sortie_")
+ */
 class SortieController extends AbstractController
 {
     /**
-     * @Route("/creer/sortie", name="creer_sortie")
+     * @Route("/create", name="create")
      */
     public function create(): Response
     {
@@ -89,7 +93,7 @@ class SortieController extends AbstractController
     }
 
     /**
-     * @Route("/annulerSortie/{id}", name="Sortie_annuler")
+     * @Route("/annulerSortie/{id}", name="annuler")
      */
     public function annuler (int $id,
                               EtatRepository $etatRepository,
