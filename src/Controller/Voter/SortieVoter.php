7d6fe56c6eca9238ffeab4dc5187e4af6aabd34c
Priscila Conchou <priscilaguedesdantas@gmail.com>
2021-09-01 17:44:39 +0200

    ajout de commentaires

--- src/Controller/Voter/SortieVoter.php
+++ src/Controller/Voter/SortieVoter.php
@@ -9,6 +9,7 @@ use Symfony\Component\Security\Core\User\UserInterface;
 
 class SortieVoter extends \Symfony\Component\Security\Core\Authorization\Voter\Voter
 {
+    // constantes = une pour chaque méthode
     const AFFICHER = "sortie_afficher";
 
     private $security;
@@ -23,6 +24,7 @@ class SortieVoter extends \Symfony\Component\Security\Core\Authorization\Voter\V
      */
     protected function supports(string $attribute, $subject)
     {
+        // ajouter la constante au array (self::NOMCONST)
         return in_array($attribute, [self::AFFICHER])
             && $subject instanceof Sortie;
     }
@@ -41,6 +43,7 @@ class SortieVoter extends \Symfony\Component\Security\Core\Authorization\Voter\V
 
         $sortie = $subject;
 
+        // il faut créer une fonction et l'appeller dans un case
         switch ($attribute) {
             case self::AFFICHER:
                 return $this->canAfficher($sortie,$user);
@@ -49,17 +52,28 @@ class SortieVoter extends \Symfony\Component\Security\Core\Authorization\Voter\V
         return false;
     }
 
+    /**
+     * Vérifie si la sortie peut être affichée
+     */
     private function canAfficher(Sortie $sortie, UserInterface $user)
     {
-        $etat = $sortie->getEtat();
+        $verifEtat = false;
 
+        // vérifie si la sortie est annulée (etat id 6)
+        $etat = $sortie->getEtat();
         if($etat->getId() == 6)
         {
-            return false;
+            $verifEtat = false;
         } else
         {
-            return true;
+            $verifEtat = true;
         }
+
+
+        // ajouter des autres verifications ici
+
+
+        return ($verifEtat);
     }
 
 
